020ef81921e1bcaa170654ad4b211da531327aaa
spivakdm <spivak.dm@gmail.com>
2020-08-28 17:04:11 +0300

    Update block_robxrecords.php

--- block_robxrecords.php
+++ block_robxrecords.php
@@ -9,27 +9,7 @@
  */
 
 
-require __DIR__ . '/vendor/autoload.php';
-$client = new \Google_Client();
-$client->setApplicationName('Google sheets connection');
-$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-$client->setAccessType('offline');
-$client->setAuthConfig(__DIR__ . '/credentials.json');
-$service = new Google_Service_Sheets($client);
-$spreadsheetID = "1Gxw5H3RnQ4DfdwKp_Ekv_D14eUrR4rHDfpacL92gnqM";
 
-$range = "records!A2:D2";
-$response = $service->spreadsheets_values->get($spreadsheetID, $range);
-$values = $response->getValues();
-
-if(!empty($values)) {
-  'No data found.\n';
-}
-else {
-  foreach ($values as $row) {
-    $link = $row[3]; // здесь записываю в переменную нужные данные
-  }
-}
 
 class block_robxrecords extends block_base {
 
@@ -40,6 +20,32 @@ class block_robxrecords extends block_base {
 
 
     function get_content(){
+
+      if ($this->content !== null) {
+        return $this->content;
+      }
+
+      require __DIR__ . '/vendor/autoload.php';
+      $client = new \Google_Client();
+      $client->setApplicationName('Google sheets connection');
+      $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+      $client->setAccessType('offline');
+      $client->setAuthConfig(__DIR__ . '/credentials.json');
+      $service = new Google_Service_Sheets($client);
+      $spreadsheetID = "1Gxw5H3RnQ4DfdwKp_Ekv_D14eUrR4rHDfpacL92gnqM";
+
+      $range = "records!A2:D2";
+      $response = $service->spreadsheets_values->get($spreadsheetID, $range);
+      $values = $response->getValues();
+
+      if(!empty($values)) {
+        'No data found.\n';
+      }
+      else {
+        foreach ($values as $row) {
+          $link = $row[3]; // здесь записываю в переменную нужные данные
+        }
+      }
 
       $this->content = new stdClass;
       $this->content->text = $link; //сюда ничего не приходит
